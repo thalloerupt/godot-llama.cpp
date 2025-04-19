912f5b77695d80edc02b4a1c85c0dcf9f522ff1f
thalloerupt <l694630313@gmail.com>
2025-04-19 14:12:34 +0800

    first commit

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo off
+REM 删除 build 文件夹（如果存在）
+if exist build (
+    rmdir /s /q build
+    echo Deleted old build folder.
+)
+
+REM 创建新的 build 文件夹
+mkdir build
+echo Created new build folder.
+
+REM 进入 build 文件夹并执行 cmake
+cd build
+cmake ..  -DLLAMA_CURL=OFF   -DGGML_VULKAN=ON
+cmake --build . --config Release
+
+REM 暂停，以便查看输出（可选）
+pause
